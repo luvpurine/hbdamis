93ab742a4ff4059c3e68d9f47f777cba42063760
luvpurine <leahbailey347@gmail.com>
2025-03-22 00:47:12 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ document.getElementById('lgButt').addEventListener('click', function(){
         document.getElementById('wrongPass').style.display = 'none';
         document.getElementById('scarlett').style.display = 'block';
     }
+    else if(inputValue == 'yejiworlddomination'){
+        document.getElementById('home').style.display = 'none';
+        document.getElementById('wrongPass').style.display = 'none';
+        document.getElementById('camila').style.display = 'block';
+    }
     else {
         document.getElementById('wrongPass').style.display = 'block';
     }
@@ -24,4 +29,10 @@ document.getElementById('scarlettImg').addEventListener('click', function(){
     console.log('a-ah~');
     document.getElementById('scarlett').style.display = 'none';
     document.getElementById('scarlett16').style.display = 'block';
+})
+
+document.getElementById('camilaImg').addEventListener('click', function(){
+    console.log('n-ngh~')
+    document.getElementById('camila').style.display = 'none';
+    document.getElementById('camila16').style.display = 'block';
 })
