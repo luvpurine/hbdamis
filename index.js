e767453d00899324b72f6643cce9f96d5c117f87
luvpurine <leahbailey347@gmail.com>
2025-06-07 19:44:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -19,6 +19,11 @@ document.getElementById('lgButt').addEventListener('click', function(){
         document.getElementById('wrongPass').style.display = 'none';
         document.getElementById('camila').style.display = 'block';
     }
+    else if (inputValue == 'umekooita'){
+        document.getElementById('home').style.display = 'none';
+        document.getElementById('wrongPass').style.display = 'none';
+        document.getElementById('addison').style.display = 'block';
+    }
     else {
         document.getElementById('wrongPass').style.display = 'block';
     }
@@ -35,4 +40,10 @@ document.getElementById('camilaImg').addEventListener('click', function(){
     console.log('n-ngh~')
     document.getElementById('camila').style.display = 'none';
     document.getElementById('camila16').style.display = 'block';
+})
+
+document.getElementById('addisonImg').addEventListener('click', function(){
+    console.log('f-faster~')
+    document.getElementById('addison').style.display = 'none';
+    document.getElementById('addison16').style.display = 'block';
 })
